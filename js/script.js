fbb3c828f6f3a67ac6e3c30ba0c1542d19c26b9d
Ahmed Eid <a.eid@yandex.com>
2017-02-26 22:17:47 +0300

    ....

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-$(function(){
-  console.log('loaded')
-})
+$(function() {
+  // smooth scroll
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+
+  $("#menu-icon").on("click", function(){
+    $("nav").slideToggle();
+    $(this).toggleClass("active");
+});
+});
